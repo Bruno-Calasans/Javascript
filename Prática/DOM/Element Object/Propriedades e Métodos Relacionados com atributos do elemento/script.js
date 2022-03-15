c8d42a2c52a6a9beaf336829c72e802aca6c920a
Houllp <83395524+Houllp@users.noreply.github.com>
2022-03-15 19:11:43 -0300

    atributos dos elementos

---
+++ Prática/DOM/Element Object/Propriedades e Métodos Relacionados com atributos do elemento/script.js
@@ -0,0 +1,64 @@
+
+    // manipulando atributos ---------------------------------------------------
+
+    // pegando o valor de um atributo
+    let exemplo1 = document.getElementById('divExemplo1')
+    let id = exemplo1.getAttribute('id')
+    console.log(id);
+
+    // pegando o atibuto na forma de node
+    let idNode = exemplo1.getAttributeNode('id')
+    console.log(idNode);
+
+    // verificando atributos
+    console.log(exemplo1.hasAttribute('class')); // false
+    console.log(exemplo1.hasAttributes()); // true
+
+    // removendo atributos usando removeAttribute
+    console.log(exemplo1);
+    console.log(exemplo1.removeAttribute('title'));
+    console.log(exemplo1);
+
+    // removendo atributo usando o removeAttributeNode
+    console.log(exemplo1);
+    let node = exemplo1.getAttributeNode('id')
+    console.log(exemplo1.removeAttributeNode(node));
+    console.log(exemplo1);
+
+    // modificando atributos
+    exemplo1.setAttribute('id', 'novoId')
+    console.log(exemplo1);
+
+    // criando atributos
+    exemplo1.setAttribute('class', 'classeExemplo1')
+    console.log(exemplo1);
+
+
+
+    
+    /*// usando a propriedade attibutes -----------------------------------------
+    let exemplo2 = document.getElementById('divExemplo2')
+    // mostrando o atributo 'attributes'
+    console.log(exemplo1.attributes);
+
+    // acessando o primeiro atributo
+    console.log(exemplo1.attributes[0]);
+    console.log(exemplo1.attributes[0].nodeType); // 2 = attribute node
+
+    // propriedades de attributes
+
+    console.log(exemplo1.attributes[0].name);
+    console.log(exemplo1.attributes[0].value);
+    console.log(exemplo1.attributes[0].specified);
+
+    // pegando o attribute node
+    console.log(exemplo1.attributes[0]); // forma normal
+    console.log(exemplo1.attributes.getNamedItem('id')); // pelo nome
+    console.log(exemplo1.attributes.item(0)); // pelo index
+
+    // deletando um attribute node
+    console.log(exemplo1.attributes.removeNamedItem('id'));
+    console.log(exemplo1.attributes);*/
+
+    
+    
