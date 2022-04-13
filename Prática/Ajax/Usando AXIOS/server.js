c746f7a7d4767232f1e3c969340f4dd2bb29bed0
Houllp <83395524+Houllp@users.noreply.github.com>
2022-04-13 04:18:15 -0300

    axios - parte 1

--- Prática/Ajax/Usando AXIOS/server.js
+++ Prática/Ajax/Usando AXIOS/server.js
@@ -15,11 +15,53 @@
     // definindo um diretório de arquivos estáticos
     app.use('/axios', express.static('axios'))
 
-    // definindo um rota para o formulário
+    // 
+    function isPar(req, res, next){
+
+        let num = null
+        if(req.params.num) num = req.params.num
+        else num = req.query.num
+
+        // caso tenha algum número
+        if(num){
+    
+            if(num % 2 == 0 && num != 0) app.locals.par = true
+            else app.locals.par = false
+        }
+
+        next()
+    }
+    app.get('/axios/*/parOuImpar', isPar, (req, res) => {
+
+    
+        if(app.locals.par) res.send({num: req.query.num, resultado: 'par'})
+        else res.send({num: req.query.num, resultado: 'ímpar'})
+
+    })
+
+    app.get('/axios/*/parOuImpar/:num', isPar, (req, res) => {
+
+ 
+        if(app.locals.par) res.send({num: req.query.num, resultado: 'par'})
+        else res.send({num: req.query.num, resultado: 'ímpar'})
+
+    })
+
+    app.post('/axios/*/formulario', (req, res) => {
+
+        let obj  = {recebido: true, ...req.body}
+        res.send(obj)
+    })
+
+    app.post('/axios/*/formulario2', (req, res) => {
+
+        let obj  = {recebidoform: true, ...req.body}
+        res.send(obj)
+    })
    
     // ligando o servidor
     app.listen(PORT, () => {
         console.log(`Servidor http://localhost:${PORT} executando`);
     })
 
-    
+   
